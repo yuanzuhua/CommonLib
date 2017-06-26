1599caa058d3e25432ec39a7e94d54c4f9e618b0
zhanghaoning <zhangbo_1226@qq.com>
2017-06-26 20:00:05 +0800

    增加程序级别日志测试

--- CommonLib/ConsoleAppTest/Program.cs
+++ CommonLib/ConsoleAppTest/Program.cs
@@ -16,8 +16,12 @@ namespace ConsoleAppTest
             Configuration
                 .Create()
                 .UseAutofac()
-                .UseLog4Net();
-
+                .UseLog4Net()
+                .RegisterUnhandledExceptionHandler();
+            //测试应用程序级别的异常日志
+            String str = null;
+            str.ToString();
+            //类级别的日志
             ILogger _logger = ObjectContainer.Resolve<ILoggerFactory>().Create(typeof(Program));
             _logger.Debug("ddd");
             _logger.Info("132342");
